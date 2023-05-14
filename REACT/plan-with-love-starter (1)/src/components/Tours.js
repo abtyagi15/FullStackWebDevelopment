6f17931748f4cf1b452f3463ff847dc41368531d
abtyagi15 <abtyagi150702@gmail.com>
2023-05-14 21:15:13 +0530

    project is completed

---
+++ REACT/plan-with-love-starter (1)/src/components/Tours.js
@@ -0,0 +1,17 @@
+import Card from "./Card";
+const Tours = ({ tours, removeTour }) => {
+
+  return (
+    <div className="container">
+      <div>
+        <h2 className="title">Plan With Love</h2>
+      </div>
+      <div className="cards">
+      {tours.map((tours) => {
+        return <Card key={tours.id}{...tours} removeTour={removeTour}></Card>;
+      })}
+    </div>
+    </div>
+  );
+}
+export default Tours;
