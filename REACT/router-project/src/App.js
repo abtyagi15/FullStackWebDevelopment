dde4729d4432f5d7b1dd1650c7cadab5c93d485c
abtyagi15 <abtyagi150702@gmail.com>
2023-05-25 22:04:25 +0530

    the project is completed

--- REACT/router-project/src/App.js
+++ REACT/router-project/src/App.js
@@ -1,7 +1,31 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import { useState } from "react";
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  return <div>Hello World</div>;
+  const [isLoggedIn,setIsLoggedIn] = useState(false);
+
+  return (
+    <div className="w-[100vw] h-[100vh] flex flex-col bg-richblack-900 ">
+        <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+        <Routes>
+          <Route path="/" element= {<Home/>} />
+          <Route path="/login" element= {<Login setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="/signup" element= {<Signup setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="/dashboard" element= {
+            <PrivateRoute isLoggedIn={isLoggedIn}>
+              <Dashboard/>
+            </PrivateRoute>
+           } />
+        </Routes>
+    </div>
+  );
 }
 
 export default App;
