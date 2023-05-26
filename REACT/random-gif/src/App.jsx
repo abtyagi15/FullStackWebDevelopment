8f13ff73e16ad76e195d6b0625c6a2e2dcb26a9e
abtyagi15 <abtyagi150702@gmail.com>
2023-05-26 13:10:13 +0530

    this is a app which can generate random gifs

---
+++ REACT/random-gif/src/App.jsx
@@ -0,0 +1,14 @@
+import Random from "./components/Random"
+import Tag from "./components/Tag"
+
+export default function App() {
+  return (
+    <div className="w-full h-screen background flex flex-col items-center">
+      <h1 className="bg-white w-11/12 text-center text-4xl rounded-lg font-bold mt-[40px] px-10 py-2">RANDOM GIF</h1>
+      <div className="flex flex-column w-full">
+        <Random />
+        <Tag />
+      </div>
+    </div>
+  );
+}
