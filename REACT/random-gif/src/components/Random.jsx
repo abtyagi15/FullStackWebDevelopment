a9f20f089ba843a79a5806f30fa677d3c9cf3d4f
abtyagi15 <abtyagi150702@gmail.com>
2023-05-27 14:53:20 +0530

    completed the project

--- REACT/random-gif/src/components/Random.jsx
+++ REACT/random-gif/src/components/Random.jsx
@@ -1,21 +1,21 @@
-import React , {useState} from "react";
+import React  from "react";
+import useGif from "../hooks/useGif";
+import Spinner from "./Spinner";
+
 
 export default function Random() {
 
-  const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
-  const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY};`
-  const [gif,setGif] = useState('');
-  function clickHandler() {
+  const {gif,loading,fetchData} = useGif();
 
-  }
 
   return (
-    <div className="flex flex-col items-center justify-center w-1/2 h-[450px] bg-green-500 border border-black rounded-md mx-auto mt-[20px]">
+    <div className="flex flex-col items-center justify-center w-1/2 bg-green-500 border border-black rounded-md mx-auto mt-[20px]">
       <h1 className="text-3xl uppercase font-bold underline">A Random Gif</h1>
-      <img src={gif} alt="gif" />
+      { loading ? (<Spinner />) : (<img src={gif} alt="gif" width="450" />) }
+      
       <button
       className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg "
-      onClick={clickHandler}
+      onClick={() => fetchData()}
       >Generate</button>
     </div>
   );
