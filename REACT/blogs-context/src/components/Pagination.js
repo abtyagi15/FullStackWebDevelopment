f3dfd4f908b606f2ae9487938dff65b217261ea5
abtyagi15 <abtyagi150702@gmail.com>
2023-05-28 21:01:35 +0530

    it include next, previous buttons functionality, code for displaying the current page out of total pages

--- REACT/blogs-context/src/components/Pagination.js
+++ REACT/blogs-context/src/components/Pagination.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useContext} from 'react'
+import { AppContext } from '../context/AppContext'
 
 const Pagination = () => {
+
+  const {page,totalPages,handlePageChange} = useContext(AppContext);
+  
+
   return (
-    <div>Pagination</div>
+    <div className='flex justify-between'>
+      <div className='flex gap-3'>
+        { page!==1 &&
+          <button onClick={() => handlePageChange(page-1)}>
+            Previous
+          </button>
+        }
+        {page!== totalPages &&
+          <button onClick={() => handlePageChange(page+1)}>
+            Next
+          </button>
+        }
+      </div>
+      <div>
+        page {page} of {totalPages}
+      </div>
+    </div>
   )
 }
 
