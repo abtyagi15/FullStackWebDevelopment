02a7279a2abce65fdb16db5e9935d4989a23a652
abtyagi15 <abtyagi150702@gmail.com>
2023-05-28 17:15:10 +0530

    Header, Blog and Pagination components added

--- REACT/blogs-context/src/App.js
+++ REACT/blogs-context/src/App.js
@@ -1,3 +1,14 @@
+import Header from "./components/Header";
+import Pagination from "./components/Pagination";
+import Blog from "./components/Blog";
+
+
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <Blog />
+      <Pagination />
+    </div>
+  );
 }
