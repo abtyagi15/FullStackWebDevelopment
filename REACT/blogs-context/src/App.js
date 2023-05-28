bb5d0cf8e3b4c6d1ff275af62aaeee931701f61d
abtyagi15 <abtyagi150702@gmail.com>
2023-05-28 20:57:03 +0530

    added useEffect hook to execute the async fetchBlogPosts function

--- REACT/blogs-context/src/App.js
+++ REACT/blogs-context/src/App.js
@@ -1,9 +1,17 @@
 import Header from "./components/Header";
 import Pagination from "./components/Pagination";
 import Blog from "./components/Blog";
+import { AppContext } from "./context/AppContext";
+import { useContext, useEffect } from "react";
+import "./App.css"
 
 
 export default function App() {
+  const {fetchBlogPosts} = useContext(AppContext);
+  useEffect(() => {
+    fetchBlogPosts();
+  },[])
+  
   return (
     <div>
       <Header />
