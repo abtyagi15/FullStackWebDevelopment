dc57061f7dd135e08c0c652f5d200c251cefad05
abtyagi15 <abtyagi150702@gmail.com>
2023-05-28 18:10:42 +0530

    using the useContext hook, loading and posts are imported from AppContext and spinner as well as card components are also added

--- REACT/blogs-context/src/components/Blog.js
+++ REACT/blogs-context/src/components/Blog.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
+import Spinner from './Spinner';
+import Card from './Card';
 
 const Blog = () => {
+
+  const {loading,posts} = useContext(AppContext);
+
   return (
-    <div>Blog</div>
+    <div>
+      { loading ? (<Spinner />) : (
+        posts.length === 0 ? 
+        (<div>
+            <p>No post found</p>
+          </div>) : 
+          (posts.map(() => (<Card />)))
+      )}
+    </div>
   )
 }
 
